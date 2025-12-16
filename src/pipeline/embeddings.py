f055274088d46f015f03df34e3fba1587ef20941
Malavika-R <13malavika13@gmail.com>
2025-12-17 00:51:56 +0530

    Initial commit: RAG-based news article analyser

--- src/pipeline/embeddings.py
+++ src/pipeline/embeddings.py
@@ -4,15 +4,15 @@ import os
 
 class CustomEmbeddings(OpenAIEmbeddings):
     def __init__(self, model_name: str = "text-embedding-3-small"):
-        api_key = os.getenv("OPENAI_API_KEY")
+        api_key = os.getenv("API_CODE")
 
         if not api_key:
             raise EnvironmentError(
-                "OPENAI_API_KEY not found. Please set it in .env or environment variables."
+                "API_CODE not found. Please set it in .env or environment variables."
             )
 
         super().__init__(
             model=model_name,
-            openai_api_key=api_key
+            api_code=api_key
         )
 
